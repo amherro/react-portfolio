5ff8a98dbdb344fc37f204d7fc5df315b4735e95
Adam Herro <adam.herro@comcast.net>
2023-03-25 13:28:17 -0400

    Fixing margin-top of technologies section

--- src/components/HomeTechnologies.jsx
+++ src/components/HomeTechnologies.jsx
@@ -11,37 +11,37 @@ const HomeTechnologies = () => {
     <div className="py-20">
         <h1 className='technology-header pb-10 text-center text-3xl font-bold'>Technologies and Skills</h1>
         <div className="grid grid-cols-2 justify-items-center">
-            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4 ">HTML</div>
                 <div className="image-container pr-5">
                     <img src={htmlImg} alt="HTML5 Logo" className='logo'/>
                 </div>
             </div>
-            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4">React</div>
                 <div className="image-container pr-5">
                     <img src={reactImg} alt="React Logo" className='logo'/>
                 </div>
             </div>
-            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4">CSS</div>
                 <div className="image-container pr-5">
                     <img src={cssImg} alt="CSS3 Logo" className='logo'/>
                 </div>
             </div>
-            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4">Javascript</div>
                 <div className="image-container pr-5">
                     <img src={jsImg} alt="Javascript Logo" className='logo'/>
                 </div>
             </div>
-            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-end card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4">GSAP Animation</div>
                 <div className="image-container pr-5">
                     <img src={gsapImg} alt="Greensock Logo" className='logo'/>
                 </div>
             </div>
-            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mb-4">
+            <div className="flex-row items-start justify-center justify-self-start card w-60 bg-zinc-800 rounded-none m-10 mt-2 mb-4">
                 <div className="card-title p-3 pl-4">Tailwind CSS</div>
                 <div className="image-container pr-5">
                     <img src={tailwindImg} alt="Tailwind CSS Logo" className='logo'/>
