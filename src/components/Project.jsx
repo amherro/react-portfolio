924adee980699f6a4caacdd703ec8d1204fabba5
Adam Herro <adam.herro@comcast.net>
2023-03-28 11:15:15 -0400

    Finishing basic modal functionality and layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 
-const Project = ({source, title, description, liveUrl, githubUrl}) => {
+const Project = ({source, title, screenshot, description, liveUrl, githubUrl}) => {
   return (
     <div>
         <div className="image-overlay card relative" data-overlay-title={title}>
             <img src={source} alt={title} className='project-screenshot w-full align-top relative'/>
-            <button className='open-modal-button card-action btn btn-primary btn-sm'>Open</button>
+            <label htmlFor='project-modal' className='open-modal-button card-action btn btn-primary btn-sm ml-5 mr-5'>Open</label>
         </div>
-        {/* Dark overlay with text on hover */}
-        {/* Button to open modal */}
 
         {/* ********* Modal ********* */}
+        <input type="checkbox" id="project-modal" className="modal-toggle" />
+        <div className="modal">
+            <div className="modal-box">
+                <label htmlFor="project-modal" className="btn btn-sm btn-circle hover:bg-red-500 hover:text-white absolute right-2 top-2">✕</label>
+                <h1 className='text-center text-3xl font-bold mb-2'>{title}</h1>
+                <a href={liveUrl}>
+                    <img src={screenshot} alt={title} className='w-3/4 self-center ml-auto mr-auto m-4' />
+                </a>
+                <p className='text-center'>{description}</p>
+                <div className="flex justify-center mt-4">
+                    <a href={liveUrl} className='btn btn-primary m-2 ml-0'>Demo</a>
+                    <a href={githubUrl} className='btn btn-primary m-2'>GitHub</a>
+                </div>
+            </div>
+        </div>
         {/* Title */}
         {/* Screenshot */}
-        {/* Short description */}
-        {/* Buttons to open live version and GitHub repo */}
     </div>
   )
 }
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -7,7 +7,7 @@ const ProjectsPage = () => {
         <div>
             <Header title='My Projects' />
             <div className="grid grid-cols-3">
-                <Project source={libraryPhoto} title='Library App' />
+                <Project source={libraryPhoto} title='Library App' screenshot={libraryPhoto} description='A library web app to keep track of all the books in your life. This project uses ES6 classes to handle the logic, as well as local storage so users can save their entries in their browser.' liveUrl='https://amherro.github.io/Library/' githubUrl='https://github.com/amherro/Library' />
             </div>
         </div>
     )
