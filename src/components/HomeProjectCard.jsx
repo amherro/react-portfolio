6a26d9c2f6e009adc8a8539d4bb88c4cb3d24290
Adam Herro <adam.herro@comcast.net>
2026-05-05 20:50:46 -0400

    Styling Project section on the homepage.

--- src/components/HomeProjectCard.jsx
+++ src/components/HomeProjectCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const HomeProjectCard = ({ title, description, tags = [] }) => {
+const HomeProjectCard = ({ title, description, tags = [], url }) => {
     return (
-        <div className='project-container'>
-            <h3 className='project-title'>{title}</h3>
-            <p>{description}</p>
-            <div>{tags.map((tag, index) =>
-                <p key={index}>{tag}</p>
-            )}</div>
-        </div>
+        <a href={url}>
+            <div className='project-container'>
+                <h3 className='project-title'>{title}</h3>
+                <p className='project-description'>{description}</p>
+                <div className='flex'>{tags.map((tag, index) =>
+                    <p key={index} className='project-tag'>{tag}</p>
+                )}</div>
+                <div className='project-arrow'>↗</div>
+            </div>
+        </a>
     )
 }
 
--- src/components/HomeProjects.jsx
+++ src/components/HomeProjects.jsx
@@ -1,4 +1,5 @@
 import HomeProjectCard from "./HomeProjectCard";
+import { Link } from "react-router-dom";
 const HomeProjects = () => {
   return (
     <section className="flex flex-col mt-10 mb-10 lg:mb-[8rem]">
@@ -18,10 +19,13 @@ const HomeProjects = () => {
         </a>
       </div>
       <div className="home-projects-grid">
-        <HomeProjectCard title="Hockey Store" description="A React, Express, and Stripe hockey store." tags={["React", "Express", "Tailwind"]} />
-        <HomeProjectCard title="Vaudify" description="Vaudify is a web tool that extracts audio from video files and downloads them as MP3s" tags={["Astro", "React", "Express", "FFmpeg", "Tailwind"]} />
-        <HomeProjectCard title="Ad Builder" description="An internal tool for my current team to develop advertisements with a user interface and AI capabilities." tags={["React", "Zustand", "Tailwind", "Claude"]} />
+        <HomeProjectCard title="Ad Builder" description="An internal tool for my current team to develop advertisements with a user interface and AI capabilities." tags={["React", "Zustand", "Tailwind", "Claude"]} url="https://ad-builder.up.railway.app/" />
+        <HomeProjectCard title="Hockey Store" description="A React, Express, and Stripe hockey store." tags={["React", "Express", "Tailwind", "Stripe"]} url="https://hockey-ecommerce-store.onrender.com/" />
+        <HomeProjectCard title="Vaudify" description="Vaudify is a web tool that extracts audio from video files and downloads them as MP3s" tags={["Astro", "React", "Express", "FFmpeg", "Tailwind"]} url="https://vaudify.com/" />
       </div>
+      <Link to="/projects">
+        <button className="home-projects-btn">View All Projects</button>
+      </Link>
 
     </section>
   );
